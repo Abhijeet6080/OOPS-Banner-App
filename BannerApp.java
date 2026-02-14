7edd301e4f95e79a0ff20cbd778aacbfaaadf0e8
Abhijeet6080 <am5185@srmist.edu.in>
2026-02-14 16:46:14 +0530

    feat: implement UC5 distance conversion km to miles

--- BannerApp.java
+++ BannerApp.java
@@ -1,12 +1,13 @@
 public class BannerApp {
     public static void main(String[] args) {
-        int maths = 94;
-        int physics = 95;
-        int chemistry = 96;
-        
-        // Calculate average using 3.0 for precision
-        double average = (maths + physics + chemistry) / 3.0;
-        
-        System.out.println("Sam's average mark in PCM is " + average);
+        int costPrice = 129;
+        int sellingPrice = 191;
+        int profit = sellingPrice - costPrice;
+        // Type conversion to double for the percentage calculation
+        double profitPercentage = (profit / (double)costPrice) * 100;
+
+        // Requirement: Single print statement for multiline output
+        System.out.println("The Cost Price is INR " + costPrice + " and Selling Price is INR " + sellingPrice + 
+                        "\nThe Profit is INR " + profit + " and the Profit Percentage is " + profitPercentage);
     }
 }
