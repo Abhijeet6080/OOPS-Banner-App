eba261a7912a084a0e1ca0c14420139f41b964f8
Abhijeet6080 <am5185@srmist.edu.in>
2026-03-02 19:26:34 +0530

    UC6: Refactor banner logic into static functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,54 +1,60 @@
 public class OOPSBannerApp {
-    public static void main(String[] args) {
-    
-        // 1. Call your helper methods to get the patterns [cite: 38, 120-122]
-        String[] oPattern = getOPattern();
-        String[] pPattern = getPPattern();
-        String[] sPattern = getSPattern();
 
-        // 2. Loop to assemble and print each line of the banner "OOPS" [cite: 40, 126]
-        for (int i = 0; i < oPattern.length; i++) {
-            // This line combines the letters with a space in between [cite: 65]
-            System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
-        }
-    }
-    // Method to generate the pattern for the letter 'O'
+    // Helper method for letter O
     public static String[] getOPattern() {
-        return new String[] {
-            "  *** ",
-            " ** ** ",
-            "** **",
-            "** **",
-            "** **",
-            " ** ** ",
-            "  *** "
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
         };
     }
 
-    // Method to generate the pattern for the letter 'P'
+    // Helper method for letter P
     public static String[] getPPattern() {
-        return new String[] {
-            "*******",
-            "** **",
-            "** **",
-            "*******",
-            "** ",
-            "** ",
-            "** "
+        return new String[]{
+                "*******  ",
+                "*      * ",
+                "*      * ",
+                "*******  ",
+                "*        ",
+                "*        ",
+                "*        "
         };
     }
 
-    // Method to generate the pattern for the letter 'S'
+    // Helper method for letter S
     public static String[] getSPattern() {
-        return new String[] {
-            " ***** ",
-            "** ",
-            "** ",
-            " ***** ",
-            "     **",
-            "** ",
-            " ***** "
+        return new String[]{
+                " ******  ",
+                "*        ",
+                "*        ",
+                " *****   ",
+                "        *",
+                "        *",
+                " ******  "
         };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o1 = getOPattern();
+        String[] o2 = getOPattern();
+        String[] p  = getPPattern();
+        String[] s  = getSPattern();
+
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("  ", o1[i], o2[i], p[i], s[i]);
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
 
