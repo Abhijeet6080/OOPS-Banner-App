68760f465a6e4b8525cb988d36b4539065b0e535
Abhijeet6080 <am5185@srmist.edu.in>
2026-03-17 22:47:38 +0530

    Added program to calculate mean height of football players

---
+++ Week_5-6/Level1/Que4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Que4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double[] heights = new double[11];
+        double sum = 0;
+        double mean;
+
+        System.out.println("Enter heights of 11 football players:");
+
+        // Taking input
+        for (int i = 0; i < heights.length; i++) {
+            heights[i] = sc.nextDouble();
+            sum = sum + heights[i];
+        }
+
+        // Calculating mean
+        mean = sum / heights.length;
+
+        System.out.println("Mean height of the football team is: " + mean);
+
+        sc.close();
+    }
+}
