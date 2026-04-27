cb49712516caf3dabad755dcbfca696d08dfa803
Abhijeet6080 <am5185@srmist.edu.in>
2026-04-27 17:39:56 +0530

    feat(UC2): implement toss logic to decide first player and assign symbols

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,12 @@
+import java.util.Random;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
 
-        // Create 3x3 board
+        // UC1: Create board
         char[][] board = new char[3][3];
 
-        // Initialize all cells with '-'
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j] = '-';
@@ -14,12 +15,36 @@ public class TicTacToe {
 
         // Display board
         System.out.println("Empty Tic-Tac-Toe Board:");
-
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
+
+        // UC2: Toss Logic
+        Random random = new Random();
+
+        boolean userStarts = random.nextBoolean();
+
+        char userSymbol;
+        char computerSymbol;
+        String currentPlayer;
+
+        if (userStarts) {
+            userSymbol = 'X';
+            computerSymbol = 'O';
+            currentPlayer = "User";
+        } else {
+            userSymbol = 'O';
+            computerSymbol = 'X';
+            currentPlayer = "Computer";
+        }
+
+        // Display toss result
+        System.out.println("\nToss Result:");
+        System.out.println("User Symbol: " + userSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+        System.out.println("First Turn: " + currentPlayer);
     }
 }
